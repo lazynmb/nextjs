363da648d858239889bdbe473959645ffaf209c8
lazynmb <dominikiwan@gmail.com>
2024-02-28 11:45:14 +0100

    index14

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,7 +1,22 @@
-import { GetServerSideProps } from 'next';
-import fetch from 'node-fetch';
+import { useState } from 'react';
 
-export default function Page({ data }: { data: any }) {
+interface DataType {
+  calcFromPairsResult: number; // Replace 'any' with the actual type of calcFromPairsResult
+  calcFromNegativePairsResult: number; // Replace 'any' with the actual type of calcFromNegativePairsResult
+  categoriesResult: number; // Replace 'any' with the actual type of categoriesResult
+}
+
+export default function Page() {
+  const [data, setData] = useState<DataType | null>(null);
+
+  if (!data) {
+    fetch('https://nextjs-liard-alpha-14.vercel.app/pages/api/calc', { cache: 'no-store' })
+      .then(response => response.json())
+      .then(fetchedData => setData(fetchedData));
+
+    return 'Loading...';
+  }
+
   return (
     <div>
       <h1>Data from calc.js:</h1>
@@ -11,15 +26,3 @@ export default function Page({ data }: { data: any }) {
     </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  // Fetch data from ./pages/api/calc.js
-  const res = await fetch('https://nextjs-liard-alpha-14.vercel.app/pages/api/calc');
-  const data = await res.json();
-
-  return {
-    props: {
-      data,
-    },
-  };
-};
